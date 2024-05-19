b9c7398a611662e14261b72fe7ffb289abdb02f9
DanielaTola <63876914+DanielaTola@users.noreply.github.com>
2024-05-18 19:20:28 -0500

    Update db.py

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
 #linea de conexion
-engine =create_engine("mysql+pymysql://daniela:123@localhost:3306/storedb")
+engine =create_engine("mysql+pymysql://user:pass@localhost:3306/name_db")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 meta = MetaData()
